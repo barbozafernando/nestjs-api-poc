d1447c88a30c57957b55ea677d41edc18dc039fa
Fernando Barboza <falandocomofernando@gmail.com>
2022-07-31 13:23:44 -0300

    refactor: add port on main (#5)

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.APP_PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   await app.listen(3000, () =>
-    Logger.log('App is running at 3000', 'NESTJS-API-POC'),
+    Logger.log(`App is running at ${port}`, 'NESTJS-API-POC'),
   );
 }
 bootstrap();
