7117b09a2f8a98d84bc9edd11743d60bcabba16c
Fernando Barboza <falandocomofernando@gmail.com>
2022-07-30 22:45:13 -0300

    refactor: remove datasource initialization on main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import dataSource from './config/postgres-data-source';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  dataSource
-    .initialize()
-    .then(() => Logger.log('Data Source has been initialized', 'DataSource'))
-    .catch(() => Logger.log('Error initializing Data Source', 'DataSource'));
-
-  await app.listen(3000, () => Logger.log('App is running at 3000', 'POC'));
+  await app.listen(3000, () =>
+    Logger.log('App is running at 3000', 'NESTJS-API-POC'),
+  );
 }
 bootstrap();
