08b0f6ac4a8244f11e90adfdc3db6c78623f86e3
Fernando Barboza <falandocomofernando@gmail.com>
2022-07-30 22:49:18 -0300

    feat: add test e2e (#4)
    * chore: remove duplicated lib and add sqlite
    
    * refactor: add http code 204 when delete product
    
    * refactor: import typeorm on app.module
    
    * refactor: remove datasource initialization on main
    
    * test: add e2e

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import configuration from '../src/config/configuration';
+import { TypeOrmConfigService } from '../src/modules/database/services/typeorm-config.service';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './modules/products/controller/product.controller';
 import { ProductService } from './modules/products/service/product.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
