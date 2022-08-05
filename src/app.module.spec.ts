3878b85062cf1afa63423482610e1a96b25aaf9e
Fernando Barboza <falandocomofernando@gmail.com>
2022-08-05 12:42:32 -0300

    test: add missing tests

---
+++ src/app.module.spec.ts
@@ -0,0 +1,24 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from './app.module';
+
+describe('AppModule', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('should be defined ', () => {
+    expect(app).toBeDefined();
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
