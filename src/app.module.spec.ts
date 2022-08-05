82cf834a1dd8bf0f22e69fceeecf9132c7726233
Fernando Barboza <falandocomofernando@gmail.com>
2022-08-05 12:55:40 -0300

    test: fix test

--- src/app.module.spec.ts
+++ src/app.module.spec.ts
@@ -1,26 +1,42 @@
+import configuration from './config/configuration';
 import { INestApplication } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { resolve } from 'path';
 import { AppModule } from './app.module';
 
-jest.setTimeout(20000);
+jest.setTimeout(40000);
+
+const SOURCE_PATH = resolve(__dirname, '..', '..');
+const PATH = `${SOURCE_PATH}/**/entities/**/*.entity.ts`;
 
 describe('AppModule', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          dropSchema: true,
+          entities: [PATH],
+        }),
+      ],
     }).compile();
 
     app = module.createNestApplication();
     await app.init();
   });
 
-  afterEach(async () => {
+  afterAll(async () => {
     await app.close();
   });
 
-  it('should be defined ', () => {
+  it('should be defined', () => {
     expect(app).toBeDefined();
   });
 });
